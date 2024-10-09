57795fef3a8b043d2e85d49181c1e0140f1891db
Tony Andy Oehme <tony-andy.oehme@tum.de>
2024-10-09 12:33:48 +0200

    automated push

--- windows.sh
+++ windows.sh
@@ -3,5 +3,6 @@ irm christitus.com/win | iex
 wsl --install -d [ your favorite linux flavor ]
 choco install GlazeWM
 choco install zebar
-choco install spotify
+git clone https://github.com/tonyOehme/.glzr.git ~/.glzr
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-Expression "& { $(Invoke-WebRequest -UseBasicParsing 'ht tps://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1') } -UninstallSpotifyStoreEdition -UpdateSpotify"
+
