bff4d2617c9d892a6634840182eb830f9f4f7576
Tony Andy Oehme <tony-andy.oehme@tum.de>
2024-10-09 19:49:45 +0200

    automated push

--- .config/nvim/lua/theprimeagen/lazy/lualine.lua
+++ .config/nvim/lua/theprimeagen/lazy/lualine.lua
@@ -55,7 +55,7 @@ return {
             sections = {
                 lualine_a = { 'mode' },
                 lualine_b = { 'branch', 'diff', 'diagnostics' },
-                lualine_c = { 'filename' },
+                lualine_c = { { 'filename', path = 1 } },
                 lualine_x = { 'encoding', 'fileformat', 'filetype' },
                 lualine_y = { 'progress' },
                 lualine_z = { 'location' }
@@ -63,7 +63,7 @@ return {
             inactive_sections = {
                 lualine_a = {},
                 lualine_b = {},
-                lualine_c = { 'filename' },
+                lualine_c = { { 'filename', path = 2 } },
                 lualine_x = { 'location' },
                 lualine_y = {},
                 lualine_z = {}
--- .config/nvim/lua/theprimeagen/lazy/oil.lua
+++ .config/nvim/lua/theprimeagen/lazy/oil.lua
@@ -5,8 +5,7 @@ return {
         require("oil").setup {
             columns = { "icon" },
             keymaps = {
-                ["<C-h>"] = false,
-                ["<M-h>"] = "actions.select_split",
+
             },
             view_options = {
                 show_hidden = true,
