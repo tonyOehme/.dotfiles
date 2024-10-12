ecdd682551c2e5419448c3972faaf2576d1c07f0
Tony Andy Oehme <tony-andy.oehme@tum.de>
2024-10-13 00:14:16 +0200

    automated push

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,9 +8,16 @@ local config = wezterm.config_builder()
 
 -- my coolnight colorscheme
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.color_scheme = 'OneDark (base16)'
-config.font_size = 17
+config.font = wezterm.font("Jetbrains Mono")
+config.color_scheme = "OneDark (base16)"
+config.font_size = 19
+config.window_padding = {
+    left = 0,
+    right = 0,
+    bottom = 0,
+    top = 0,
+}
+
 
 config.enable_tab_bar = false
 
