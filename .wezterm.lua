e84f22f02f241c97e966c2c7b65d0c9e5a940d4c
Tony Andy Oehme <tony-andy.oehme@tum.de>
2024-11-04 01:33:21 +0100

    automated push

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,7 +8,7 @@ local config = wezterm.config_builder()
 
 -- my coolnight colorscheme
 
-config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
+config.font = wezterm.font("JetBrainsMonoNL Nerd Font Propo")
 config.color_scheme = "OneDark (base16)"
 config.font_size = 19
 config.window_padding = {
