c27438449fcd0771eaaa8dfcbcefbfe7be1e454f
Tony Andy Oehme <tony-andy.oehme@tum.de>
2024-11-15 03:00:58 +0100

    automated push

--- nix/macOS/home.nix
+++ nix/macOS/home.nix
@@ -19,12 +19,12 @@ in
 
   programs = {
     home-manager.enable = true;
-    tmux = import ../shared/tmux.nix { inherit pkgs; };
-    zsh = import ../shared/zsh.nix { inherit config pkgs lib; };
-    zoxide = import ../shared/zoxide.nix { inherit config pkgs; };
-    alacritty = import ../shared/alacritty.nix { inherit pkgs; };
-    fzf = import ../shared/fzf.nix { inherit pkgs; };
-    git = import ../shared/git.nix { inherit pkgs config; };
-    wezterm = import ../shared/wezterm.nix { inherit pkgs config; };
+    tmux = import ../shared/tmux.nix { inherit pkgs lib config; };
+    zsh = import ../shared/zsh.nix { inherit pkgs lib config; };
+    zoxide = import ../shared/zoxide.nix { inherit config lib pkgs; };
+    alacritty = import ../shared/alacritty.nix { inherit pkgs lib config; };
+    fzf = import ../shared/fzf.nix { inherit pkgs lib config; };
+    git = import ../shared/git.nix { inherit pkgs lib config; };
+    wezterm = import ../shared/wezterm.nix { inherit pkgs lib config; };
   };
 }
