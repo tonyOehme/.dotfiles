ca2770fa4b7b718bf76f127f2a686ed012393327
Tony Andy Oehme <tony-andy.oehme@tum.de>
2024-11-19 14:33:36 +0100

    automated push

--- nix/shared/git.nix
+++ nix/shared/git.nix
@@ -3,6 +3,5 @@
   enable = true;
   lfs.enable = true;
   ignores = lib.filter (line: !builtins.isList line) (lib.strings.split "\n" (builtins.readFile ../../.config/git/ignore));
-
   extraConfig = builtins.fromTOML (builtins.readFile ../../.config/git/conf);
 }
---
+++ nix/shared/kitty.nix
@@ -0,0 +1,6 @@
+{ pkgs, config, lib, ... }: {
+
+
+
+    enable = true;
+}
